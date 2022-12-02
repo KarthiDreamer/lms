81a16f29da96081464c31244f3a6bb6bfee69938
Karthi Keyan <karthikeyan.jayhind@gmail.com>
2022-12-02 15:01:12 +0530

    single

--- library/models.py
+++ library/models.py
@@ -3,5 +3,16 @@ from django.db import models
 class student(models.Model):
     id=models.AutoField(primary_key=True,unique=True)
     studentid= models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
 
+class admin1(models.Model):
+    id=models.AutoField(primary_key=True,unique=True)
+    adminid= models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+
+class books(models.Model):
+    id=models.AutoField(primary_key=True,unique=True)
+    bookid=models.CharField(max_length=100)
+    bookname=models.CharField(max_length=100)
+    bookstate=models.BooleanField()
 
